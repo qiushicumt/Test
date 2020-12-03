5b0595a2f9324df1de4626794ae502beb249c0f0
qiushicumt <qiushicumt@gmail.com>
2020-12-03 23:49:56 +0800

    selenium_day07_1.0.4
    selenium_day07_1.0.4

--- code/selenium/selenium_day07/unit_demo/unit_readdata_for_test.py
+++ code/selenium/selenium_day07/unit_demo/unit_readdata_for_test.py
@@ -1,68 +1,59 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 """
-@author: Administrator
-@file: unit_readdata_for_test
-@time: 2020/12/3
+@author:frankzhong
+@file:unit_readdata_for_test.py
+@time:2020/12/02
 """
+from selenium import webdriver
 import unittest
 import time
-from selenium import webdriver
-from ddt import ddt, data, unpack
+from ddt import data,unpack,ddt
 
 def read_data():
     """
-    读取文件中内容
-    :return: 返回读取的内容
+    读取外部数据
+    :return: 返回一个列表
     """
-    read_file = open("read_data.txt", "r", encoding="utf8")
-    data_list = []
+    read_file = open("data.txt", "r", encoding="utf8")
+    li = []
     for one in read_file.readlines():
-        data_list.append(one.strip("\n").split(","))
-    return data_list
+        li.append(one.strip("\n").split(","))
+    read_file.close()
+    return li
 
-# 给类添加ddt装饰器，表示类中会使用ddt处理数据
+
 @ddt
-class UnitReadDataForTest(unittest.TestCase):
+class UnitReadDateTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        """
-        类前置条件
-        :return: None
-        """
         pass
 
     @classmethod
     def tearDownClass(cls) -> None:
-        """
-        类后置条件
-        :return: None
-        """
         pass
 
     def setUp(self) -> None:
-        """
-        测试用例函数的前置条件
-        :return:
-        """
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
-        self.driver.get("http://localhost:8088/shopxo/public/index.php?s=/index/user/logininfo.html")
+        self.driver.get("http://192.168.0.105:8088/shopxo/index.php?s=/index/user/logininfo.html")
 
     def tearDown(self) -> None:
-        """
-        测试用例函数的后置条件
-        :return:
-        """
-        time.sleep(3)       # 关闭窗口前暂停3秒
-        self.driver.quit()  # 关闭窗口
+        time.sleep(3)
+        self.driver.quit()
 
     @data(*read_data())
     @unpack
-    def test_Login(self, username, passwd):
+    def test_readDataForTest(self, username, passwd):
+        """
+        测试通过读取外部数据登录
+        :param username: 用户名
+        :param passwd: 密码
+        :return: None
+        """
         self.driver.find_element_by_name("accounts").send_keys(username)
         self.driver.find_element_by_name("pwd").send_keys(passwd)
         self.driver.find_element_by_css_selector(".form-validation button").click()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main(verbosity=2)
